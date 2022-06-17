7015ac94294ebad04c22124d83409898db38bea9
anass <anass.dakhouch@outlook.com>
2022-06-17 14:41:17 +0100

    sixth commit

--- src/main/java/com/pfa/sudodental/config/MvcConfig.java
+++ src/main/java/com/pfa/sudodental/config/MvcConfig.java
@@ -10,6 +10,8 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/eror404").setViewName("error/404");
+        registry.addViewController("/eror500").setViewName("error/500");
     }
 
 }
